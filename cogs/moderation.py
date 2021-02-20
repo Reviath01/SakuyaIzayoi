56c7128f77c6a411c4faaf1942b1e20c484f3684
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-02-20 05:54:09 +0000

    Added mysql connection and welcome_channel command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import json
+import mysql.connector
 
 class Moderation(commands.Cog):
 
@@ -11,7 +12,7 @@ class Moderation(commands.Cog):
     async def on_ready(self):
         print('Moderation commands are loaded!')
 
-    @commands.command(brief="Ban the user", description="Allow's you to ban the user")
+    @commands.command(brief="Ban the user.", description="Allow's you to ban the user.")
     @commands.has_permissions(ban_members = True)
     async def ban(self, ctx, member:discord.User = None, reason = None):
         if member == None:
@@ -23,9 +24,9 @@ class Moderation(commands.Cog):
         if reason == None:
             reason = "Unspecified"
         await ctx.guild.ban(member, reason=reason)
-        await ctx.send(f"{member} is banned!")
+        await ctx.send(f"Banned {member}!")
 
-    @commands.command(brief="Kick the user", description="Allow's you to kick the user")
+    @commands.command(brief="Kick the user.", description="Allow's you to kick the user.")
     @commands.has_permissions(kick_members = True)
     async def kick(self, ctx, member:discord.User = None, reason = None):
         if member == None:
@@ -37,24 +38,24 @@ class Moderation(commands.Cog):
         if reason == None:
             reason = "Unspecified"
         await ctx.guild.kick(member, reason=reason)
-        await ctx.send(f"{member} is kicked!")
+        await ctx.send(f"Kicked {member}!")
 
-    @commands.command(brief="Unban the user", description="Unban's the user")
+    @commands.command(brief="Unban the user.", description="Unban's the user.")
     @commands.has_permissions(ban_members = True)
     async def unban(self, ctx, *, member = None):
         if member == None:
-            await ctx.send('You need to specify a ID')
+            await ctx.send('You need to specify a ID.')
             return
         user = await self.client.fetch_user(member)
         await ctx.guild.unban(user)
-        await ctx.send(f'Unbanned <@{user.id}>')
+        await ctx.send(f'Unbanned <@{user.id}>.')
         return
 
-    @commands.command(brief="Start a vote", description="Start a vote")
+    @commands.command(brief="Start a vote.", description="Start a vote.")
     @commands.has_permissions(manage_messages = True)
     async def start_vote(self, ctx, *, message = None):
         if message == None:
-            await ctx.send('You need to say why are you starting a vote')
+            await ctx.send('You need to say why are you starting a vote.')
             return
         voteembed = discord.Embed(colour=discord.Colour.blue(), title=f"Vote started (by {ctx.author.display_name})", description=message)
         msg = await ctx.send(embed = voteembed)
@@ -63,17 +64,17 @@ class Moderation(commands.Cog):
         await msg.add_reaction(emoji)
         await msg.add_reaction(emoji2)
 
-    @commands.command(brief="Deletes messages", description="Deletes messages")
+    @commands.command(brief="Deletes messages.", description="Deletes messages.")
     @commands.has_permissions(manage_messages=True)
     async def clear(self, ctx, amount : int):
         await ctx.channel.purge(limit=amount + 1)
         await ctx.send(f'Cleared {amount} messages.')
 
-    @commands.command(brief="Allows you to set prefix", description="Allows you to set prefix")
+    @commands.command(brief="Allows you to set prefix.", description="Allows you to set prefix.")
     @commands.has_permissions(administrator=True)
     async def prefix(self, ctx, *, prefix=None):
         if prefix == None:
-            await ctx.send('You need to send the new prefix')
+            await ctx.send('You need to send new prefix.')
             return
         with open('prefixes.json', 'r') as f:
             prefixes = json.load(f)
@@ -82,7 +83,51 @@ class Moderation(commands.Cog):
 
         with open('prefixes.json', 'w') as f:
             json.dump(prefixes, f, indent=4)
-        await ctx.send('Succesfully changed prefix')
+        await ctx.send('Succesfully changed prefix.')
+
+    @commands.command(brief="Sets welcome channel.", description="Sets welcome channel as mentioned channel.")
+    async def welcome_channel(self, ctx, channel : discord.TextChannel):
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="sakuya"
+        )
+        mycursor = mydb.cursor()
+        chid = f"SELECT chid FROM welcomech WHERE serverid ='{ctx.guild.id}'"
+        mycursor.execute(chid)
+        myresult = mycursor.fetchall()
+        if myresult:
+            for x in myresult:
+                y = str(x)[:-3][-18:]
+                await ctx.send(f"Channel is already setted to <#{y}>, use reset_welcome_channel command to reset.")
+                return
+        else:
+            sql = "INSERT INTO welcomech (chid, serverid) VALUES (%s, %s)"
+            val = (channel.id, ctx.guild.id)
+            mycursor.execute(sql, val)
+            mydb.commit()
+            await ctx.send(f'Setting new welcome channel as {channel.mention}.')
+
+    @commands.command(brief="Resets welcome channel.", description="Resets welcome channel.")
+    async def reset_welcome_channel(self, ctx):
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="sakuya"
+        )
+        mycursor = mydb.cursor()
+        chid = f"SELECT chid FROM welcomech WHERE serverid ='{ctx.guild.id}'"
+        mycursor.execute(chid)
+        myresult = mycursor.fetchall()
+        if myresult:
+            await ctx.send('Resetting welcome_channel.')
+            reset = f"DELETE FROM welcomech WHERE serverid ='{ctx.guild.id}'"
+            mycursor.execute(reset)
+            mydb.commit()
+        else:
+            await ctx.send('Welcome channel is not setted.')
 
 def setup(client):
     client.add_cog(Moderation(client))
